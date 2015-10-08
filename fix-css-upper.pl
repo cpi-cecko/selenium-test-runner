1269e7791c8f3c4b1d51e7fd170454cde680ffef
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-08 22:29:17 +0300

    Adds fix-all script. fix-css-upper.pl respects the link cmd.

--- fix-css-upper.pl
+++ fix-css-upper.pl
@@ -7,33 +7,34 @@ use v5.012;
 use Encode;
 
 
-my $to_fix = qr/driver\.findElement\(By\.linkText\(\"(.+)\"\)\).+;/;
+my $to_fix = qr/driver\.findElement\(By\.linkText\(\"(.+)\"\)\)\.(.+);/;
 
 while (<STDIN>) {
   if ($_ =~ $to_fix) {
     my $text = Encode::decode('UTF-8', $1);
+    my $cmd = $2;
     my $fixed = 
       "    {\n" .
       "      WebElement link;\n" .
       "      try {\n" .
-      "        link = driver.findElement(By.xpath(\"//a[text()=\'".$text."\']\"));\n" .
+      "        link = driver.findElement(By.xpath(\"//a[text()=\'$text\']\"));\n" .
       "        try {\n" .
-      "          link.click();\n" .
+      "          link.$cmd;\n" .
       "        } catch(ElementNotVisibleException ex) {\n" .
       "          try {\n" .
-      "            link = driver.findElement(By.linkText(\"".$text."\"));\n" .
+      "            link = driver.findElement(By.linkText(\"$text\"));\n" .
       "          } catch(NoSuchElementException ex2) {\n" .
       "            link = driver.findElement(By.linkText(\"".uc($text)."\"));\n" .
       "          }\n" .
-      "          link.click();\n" .
+      "          link.$cmd;\n" .
       "        }\n" .
       "      } catch(NoSuchElementException ex) {\n" .
       "        try {\n" .
-      "          link = driver.findElement(By.linkText(\"".$text."\"));\n" .
+      "          link = driver.findElement(By.linkText(\"$text\"));\n" .
       "        } catch(NoSuchElementException ex2) {\n" .
       "          link = driver.findElement(By.linkText(\"".uc($text)."\"));\n" .
       "        }\n" .
-      "        link.click();\n" .
+      "        link.$cmd;\n" .
       "      }\n" .
       "    }\n";
     print $fixed;
