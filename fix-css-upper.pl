3e5cd1ad88514753bf666601fe77b10518c63787
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-08 21:37:55 +0300

    Java formatter and fix-css-upper.

---
+++ fix-css-upper.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.012;
+
+use Encode;
+
+
+my $to_fix = qr/driver\.findElement\(By\.linkText\(\"(.+)\"\)\).+;/;
+
+while (<STDIN>) {
+  if ($_ =~ $to_fix) {
+    my $text = Encode::decode('UTF-8', $1);
+    my $fixed = 
+      "    {\n" .
+      "      WebElement link;\n" .
+      "      try {\n" .
+      "        link = driver.findElement(By.xpath(\"//a[text()=\'".$text."\']\"));\n" .
+      "        try {\n" .
+      "          link.click();\n" .
+      "        } catch(ElementNotVisibleException ex) {\n" .
+      "          try {\n" .
+      "            link = driver.findElement(By.linkText(\"".$text."\"));\n" .
+      "          } catch(NoSuchElementException ex2) {\n" .
+      "            link = driver.findElement(By.linkText(\"".uc($text)."\"));\n" .
+      "          }\n" .
+      "          link.click();\n" .
+      "        }\n" .
+      "      } catch(NoSuchElementException ex) {\n" .
+      "        try {\n" .
+      "          link = driver.findElement(By.linkText(\"".$text."\"));\n" .
+      "        } catch(NoSuchElementException ex2) {\n" .
+      "          link = driver.findElement(By.linkText(\"".uc($text)."\"));\n" .
+      "        }\n" .
+      "        link.click();\n" .
+      "      }\n" .
+      "    }\n";
+    print $fixed;
+  } else {
+    print $_;
+  }
+}
