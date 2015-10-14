ef30b5cddf8145e4de474c801ef6d7512c8dc846
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-14 16:53:01 +0300

    Adds fix-all script, updates README.

---
+++ fix-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tests_dir="$1"
+
+if [ -z $tests_dir ]
+then
+  echo "Usage: fix-all.sh <tests_dir>"
+  exit
+fi
+
+for test_file in "$tests_dir"/*
+do
+    test_file_fixed="${test_file/.pl/_fixed.pl}"
+    ./make-bench.pl < "$test_file" > "$test_file_fixed"
+    rm $test_file
+    mv $test_file_fixed $test_file
+done
