bf5f8d75f5d6901d20003894776cbdf4d45bb650
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-15 21:31:35 +0300

    Fixes problem with dynamically generated HTML.

--- make-bench.pl
+++ make-bench.pl
@@ -4,15 +4,15 @@ use strict;
 use warnings;
 use v5.012;
 
-use Encode;
 
-
-my $to_fix = qr/(\$driver->find_element.*)/;
+my $to_fix = qr/^(\$driver->find_element.*)/;
+my $to_pause = qr/^(# ERROR:.*Unsupported command \[getAllFields .*)/; 
 
 while (<STDIN>) {
   if ($_ =~ $to_fix) {
-    my $fixed =  "benchit(sub { $1 });\n";
-    print $fixed;
+    print "benchit(sub { $1 });\n";
+  } elsif ($_ =~ $to_pause) {
+    print "\$driver->pause(500);\n";
   } else {
     print $_;
   }
