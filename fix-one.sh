de07ec4f55eac4088c9c3c766b13813dee9d2ba5
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-17 17:08:21 +0300

    Script to fix only one file.

---
+++ fix-one.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+test_script="$1"
+
+if [ -z $test_script ]
+then
+  echo "Usage: fix-one.sh <test_script>"
+  exit
+fi
+
+if [ -d $test_script ]
+then
+  echo "[$test_script] is a directory"
+  exit
+fi
+
+test_script_fixed="${test_script/.pl/_fixed.pl}"
+./make-bench.pl < "$test_script" > "$test_script_fixed"
+rm $test_script
+mv $test_script_fixed $test_script
